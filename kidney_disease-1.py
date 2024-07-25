7821e1da01a5966b4c40e5def36f9b85a4dc090d
Eric03 <ibeleric03@gmail.com>
2024-07-25 16:56:32 +0200

    uploag .py

--- kidney_disease-1.py
+++ kidney_disease-1.py
@@ -7,71 +7,84 @@ from sklearn.utils import resample
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 
-# Charger le dataset
-url = "https://raw.githubusercontent.com/Ieric03/dataset/main/kidney_disease.csv"
-df_origine = pd.read_csv(url)
+def load_data():
+    # Charger le dataset
+    url = "https://raw.githubusercontent.com/Ieric03/dataset/main/kidney_disease.csv"
+    df = pd.read_csv(url)
 
-# Suppression de la colonne "id" pas utile
-df_origine.drop("id", axis=1, inplace=True)
+    # Suppression de la colonne "id" pas utile
+    df.drop("id", axis=1, inplace=True)
 
-# Renommer les colonnes pour correspondre à celles utilisées précédemment
-column_mapping = {
-    "age": "Age", "bp": "Blood Pressure", "sg": "Specific Gravity",
-    "al": "Albumin", "su": "Sugar", "rbc": "Red Blood Cells",
-    "pc": "Pus Cell", "pcc": "Pus Cell Clumps", "ba": "Bacteria",
-    "bgr": "Blood Glucose Random", "bu": "Blood Urea", "sc": "Serum Creatinine",
-    "sod": "Sodium", "pot": "Potassium", "hemo": "Hemoglobin",
-    "pcv": "Packed Cell Volume", "wc": "White Blood Cell Count", "rc": "Red Blood Cell Count",
-    "htn": "Hypertension", "dm": "Diabetes Mellitus", "cad": "Coronary Artery Disease",
-    "appet": "Appetite", "pe": "Pedal Edema", "ane": "Anemia", "classification": "Classification"
-}
-df_origine.rename(columns=column_mapping, inplace=True)
+    # Renommer les colonnes pour correspondre à celles utilisées précédemment
+    column_mapping = {
+        "age": "Age", "bp": "Blood Pressure", "sg": "Specific Gravity",
+        "al": "Albumin", "su": "Sugar", "rbc": "Red Blood Cells",
+        "pc": "Pus Cell", "pcc": "Pus Cell Clumps", "ba": "Bacteria",
+        "bgr": "Blood Glucose Random", "bu": "Blood Urea", "sc": "Serum Creatinine",
+        "sod": "Sodium", "pot": "Potassium", "hemo": "Hemoglobin",
+        "pcv": "Packed Cell Volume", "wc": "White Blood Cell Count", "rc": "Red Blood Cell Count",
+        "htn": "Hypertension", "dm": "Diabetes Mellitus", "cad": "Coronary Artery Disease",
+        "appet": "Appetite", "pe": "Pedal Edema", "ane": "Anemia", "classification": "Classification"
+    }
+    df.rename(columns=column_mapping, inplace=True)
 
-# Nettoyage des valeurs 'ckd\t' dans la colonne 'classification'
-df_origine['Classification'] = df_origine['Classification'].str.strip()
+    # Nettoyage des valeurs 'ckd\t' dans la colonne 'classification'
+    df['Classification'] = df['Classification'].str.strip()
 
-# Gestion des valeurs manquantes
-imputer = SimpleImputer(strategy='mean')
-for column in df_origine.columns:
-    if df_origine[column].dtype == 'object':
-        df_origine[column].fillna(df_origine[column].mode()[0], inplace=True)
-    else:
-        df_origine[column] = imputer.fit_transform(df_origine[[column]])
+    return df
 
-# Transformation des variables catégorielles
-label_encoders = {}
-for column in df_origine.select_dtypes(include=['object']).columns:
-    le = LabelEncoder()
-    df_origine[column] = le.fit_transform(df_origine[column])
-    label_encoders[column] = le
+def preprocess_data(df):
+    # Gestion des valeurs manquantes
+    imputer = SimpleImputer(strategy='mean')
+    for column in df.columns:
+        if df[column].dtype == 'object':
+            df[column].fillna(df[column].mode()[0], inplace=True)
+        else:
+            df[column] = imputer.fit_transform(df[[column]])
 
-# Normalisation des données
-scaler = StandardScaler()
-scaled_columns = df_origine.columns[df_origine.dtypes != 'object']
-df_origine[scaled_columns] = scaler.fit_transform(df_origine[scaled_columns])
+    # Transformation des variables catégorielles
+    label_encoders = {}
+    for column in df.select_dtypes(include=['object']).columns:
+        le = LabelEncoder()
+        df[column] = le.fit_transform(df[column])
+        label_encoders[column] = le
 
-# Conversion de la colonne 'classification' en binaire
-df_origine['Classification'] = df_origine['Classification'].apply(lambda x: 1 if x == 'ckd' else 0)
+    # Normalisation des données
+    scaler = StandardScaler()
+    scaled_columns = df.columns[df.dtypes != 'object']
+    df[scaled_columns] = scaler.fit_transform(df[scaled_columns])
 
-# Vérification des valeurs uniques dans la colonne 'Classification'
-st.write("Valeurs uniques dans la colonne 'Classification' :", df_origine['Classification'].unique())
+    # Conversion de la colonne 'classification' en binaire
+    df['Classification'] = df['Classification'].apply(lambda x: 1 if x == 'ckd' else 0)
 
-# Vérification des classes
-if df_origine['Classification'].nunique() != 2:
-    st.error("La colonne 'Classification' doit contenir exactement deux classes (0 et 1).")
+    return df, label_encoders, scaler
 
-# Gestion du déséquilibre des classes
-df_majority = df_origine[df_origine.Classification == 0]
-df_minority = df_origine[df_origine.Classification == 1]
+def main():
+    st.title("Prédiction de Maladie Rénale Chronique")
 
-# Vérifiez les tailles des classes
-st.write("Taille de la classe majoritaire :", df_majority.shape)
-st.write("Taille de la classe minoritaire :", df_minority.shape)
+    df_origine = load_data()
+    df_origine, label_encoders, scaler = preprocess_data(df_origine)
 
-if df_majority.empty or df_minority.empty:
-    st.error("Il doit y avoir au moins un exemple dans chaque classe.")
+    # Vérification des valeurs uniques dans la colonne 'Classification'
+    st.write("Valeurs uniques dans la colonne 'Classification' :", df_origine['Classification'].unique())
 
-if not df_majority.empty and not df_minority.empty:
+    # Vérification des classes
+    if df_origine['Classification'].nunique() != 2:
+        st.error("La colonne 'Classification' doit contenir exactement deux classes (0 et 1).")
+        return
+
+    # Gestion du déséquilibre des classes
+    df_majority = df_origine[df_origine.Classification == 0]
+    df_minority = df_origine[df_origine.Classification == 1]
+
+    # Vérifiez les tailles des classes
+    st.write("Taille de la classe majoritaire :", df_majority.shape)
+    st.write("Taille de la classe minoritaire :", df_minority.shape)
+
+    if df_majority.empty or df_minority.empty:
+        st.error("Il doit y avoir au moins un exemple dans chaque classe.")
+        return
+
     df_minority_upsampled = resample(df_minority,
                                      replace=True,     # échantillonnage avec remplacement
                                      n_samples=len(df_majority),    # pour correspondre au nombre de la classe majoritaire
@@ -91,8 +104,6 @@ if not df_majority.empty and not df_minority.empty:
 
     # Préparation des valeurs moyennes pour préremplir les champs
     mean_values = df_origine.mean()
-
-    st.title("Prédiction de Maladie Rénale Chronique")
 
     # Formulaire pour entrer les valeurs des variables
     age = st.number_input("Age", value=float(mean_values["Age"]))
@@ -152,3 +163,6 @@ if not df_majority.empty and not df_minority.empty:
         st.write("Maladie Rénale Chronique" if prediction[0] == 1 else "Pas de Maladie Rénale Chronique")
         st.write("Probabilité :")
         st.write(prediction_proba)
+
+if __name__ == "__main__":
+    main()
